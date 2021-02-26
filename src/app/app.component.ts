7bd4d77ec44410d2f6f2088b9d9628a0f34bdc2e
RIhards Adminis <rihardsadminis@RIhardss-iMac.local>
2021-02-26 18:24:25 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StorageService } from './services/storage.service';
+import { ColorEvent } from 'ngx-color';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bg-changer';
+  colors = ['#f54266', '#4257f5', '#34d97e', '#d93476', '#e3e3e3'];
+  bgColor = '#d93476';
+  colorsStorageKey = 'Background Color';
+
+  constructor(private storageService: StorageService) { 
+    this.bgColor=
+    storageService.getData(this.colorsStorageKey) || this.bgColor;
+  }
+  
+  bgChange(color){
+    this.storageService.setData(this.colorsStorageKey, color);
+    this.bgColor = this.storageService.getData(this.colorsStorageKey)
+  }
+
+  handleChange($event: ColorEvent) {
+    this.storageService.setData(this.colorsStorageKey, $event.color.hex);
+    this.bgColor = this.storageService.getData(this.colorsStorageKey)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+
+import { ColorSketchModule } from 'ngx-color/sketch';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    ColorSketchModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
 })
 export class AppModule { }
